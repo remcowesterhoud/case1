f2ab8ef695b3ef207d849b5bdf0550bd239f0268
Remco Westerhoud <remcowesterhoud@msn.com>
2015-10-04 16:06:32 +0200

    Libraries for file sending added Example for file uploading added (remove later)

--- JavaMinor Case1/src/main/java/CourseAdministration/Controllers/Test.java
+++ JavaMinor Case1/src/main/java/CourseAdministration/Controllers/Test.java
@@ -1,7 +1,7 @@
 package CourseAdministration.Controllers;
 
 import com.sun.jersey.core.header.FormDataContentDisposition;
-//import com.sun.jersey.multipart.FormDataParam;
+import com.sun.jersey.multipart.FormDataParam;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
@@ -20,30 +20,33 @@ public class Test {
         return Response.status(200).entity("Test123").build();
     }
 
-//    @POST
-//    @Path("/upload")
-//    @Consumes(MediaType.MULTIPART_FORM_DATA)
-//    public Response fileTest(@FormDataParam("file")InputStream uploadInputStream, @FormDataParam("file")FormDataContentDisposition fileDetail){
-//        String uploadedFileLocation = "c://uploadedFiles/" + fileDetail.getFileName();
-//
-//        saveToFile(uploadInputStream, uploadedFileLocation);
-//        String output = "File uploaded via Jersey based RESTful Webservice to: " + uploadedFileLocation;
-//
-//        return Response.status(200).entity(output).build();
-//    }
-//
-//    private void saveToFile(InputStream uploadedInputStream, String uploadedFileLocation){
-//        try {
-//            OutputStream out = null;
-//            int read = 0;
-//            byte[] bytes = new byte[1024];
-//
-//            out = new FileOutputStream(new File(uploadedFileLocation));
-//            while ((read = uploadedInputStream.read(bytes)) != -1){
-//                out.write(bytes, 0, read);
-//            }
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//    }
+    @POST
+    @Path("/upload")
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    public String uploadFile(@FormDataParam("file") InputStream uploadedInputStream, @FormDataParam("file") FormDataContentDisposition fileDetail){
+
+        saveToDisk(uploadedInputStream, fileDetail);
+
+        return "File saved successfully";
+    }
+
+    private void saveToDisk(InputStream uploadedInputStream, FormDataContentDisposition fileDetail){
+
+        String uploadedFileLocation = "C://Users/Remco/Desktop/" + fileDetail.getFileName();
+
+        try{
+            OutputStream out = new FileOutputStream(new File(uploadedFileLocation));
+            int read = 0;
+            byte[] bytes = new byte[1024];
+
+            while ((read = uploadedInputStream.read(bytes)) != -1){
+                out.write(bytes, 0, read);
+            }
+            out.flush();
+            out.close();
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+    }
 }
